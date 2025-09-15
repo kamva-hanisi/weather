aa2cf05e2ddf1912da97f0129a70484305280c5f
KAMVA <lucashanisi@gmail.com>
2025-09-15 21:48:42 +0200

    Refactor globe.js to update date format and implement weather fetching; add new weather icons and remove unused image

--- script/globe.js
+++ script/globe.js
@@ -25,4 +25,79 @@ let months = new Date().getMonth();
 let year = new Date().getFullYear();
 
 let FullDate = document.querySelector(".date");
-FullDate.innerHTML = `${months_name[months]} ${date}, ${year}`;
+FullDate.innerHTML = `${date} ${months_name[months]} ${year}`;
+
+// Weather info
+async function city(cityName) {
+  let url = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=${apiKey}`
+  );
+  if (url.ok) {
+    let data = await url.json();
+    console.log(data);
+
+    let cityBox = document.querySelector(".city-box");
+
+    if (!box) {
+      box = document.createElement("div");
+      box.className = "box";
+      cityBox.appendChild(box);
+    }
+
+    let weatherBox = document.createElement("div");
+    weatherBox.className = "weather-box";
+
+    let nameDiv = document.createElement("div");
+    nameDiv.className = "name";
+
+    let cityElement = document.createElement("div");
+    cityElement.className = "city-name city";
+    cityElement.innerHTML = data.name;
+
+    let tempElement = document.createElement("div");
+    tempElement.className = "weather-temp temp";
+    tempElement.innerHTML = Math.floor(data.main.temp) + "°";
+
+    let weatherIconDiv = document.createElement("div");
+    weatherIconDiv.className = "weather-icon";
+
+    let weatherImg = document.createElement("img");
+    weatherImg.className = "weather";
+
+    if (data.weather[0].main === "Rain") {
+      weatherImg.src = "images/rain.png";
+    } else if (
+      data.weather[0].main === "Clear" ||
+      data.weather[0].main === "Clear Sky"
+    ) {
+      weatherImg.src = "images/sun.png";
+    } else if (data.weather[0].main === "Snow") {
+      weatherImg.src = "images/snow.png";
+    } else if (
+      data.weather[0].main === "Clouds" ||
+      data.weather[0].main === "Smoke"
+    ) {
+      weatherImg.src = "images/cloud.png";
+    } else if (
+      data.weather[0].main === "Mist" ||
+      data.weather[0].main === "Fog"
+    ) {
+      weatherImg.src = "images/mist.png";
+    } else if (data.weather[0].main === "Sky") {
+      weatherImg.src = "images/sky.png";
+    } else if (data.weather[0].main === "Thunderstorm") {
+      weatherImg.src = "images/thunderstorm.png";
+    }
+
+    weatherIconDiv.appendChild(weatherImg);
+    nameDiv.appendChild(cityElement);
+    nameDiv.appendChild(tempElement);
+    weatherBox.appendChild(nameDiv);
+    weatherBox.appendChild(weatherIconDiv);
+    box.appendChild(weatherBox);
+
+    return weatherBox;
+  } else {
+    return "";
+  }
+}
